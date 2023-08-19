d1fd6d4dd553dc17e6f815ad5aefd2cbad7bf4ee
lugumedeiros <140826713+lugumedeiros@users.noreply.github.com>
2023-08-18 23:51:16 -0300

    Update 100DBootCamp_PasswordGenerator.py
    I forgot to pass password as string. Fixed!

--- 100DBootCamp_PasswordGenerator.py
+++ 100DBootCamp_PasswordGenerator.py
@@ -2,7 +2,7 @@
 """As usual in the 100 days bootcamp from Udemy, everyday we get a project for us to
 finish. In this one my personal objective was to make all code using basic random method,
 I personally like to do things this way, for me, it feels like I'm learning more.
-There's better ways to random everything like using choice method, but what is
+There's better ways to random everything like using choice or shuffle method, but what is
 the fun in using it? lol.
 Maybe my next random code will be made from scratch using something simple
 like middle square algorithm, but I don't know yet how to implement a code
@@ -26,7 +26,7 @@ nTimes = is the amount of characters of a list(nr_letters, nr_symbols, nr_number
 list_choice = is the list of characters used (letters, numbers, symbols)'''
 
 def randomKey(nTimes, list_choice):
-#def that will create a new list with N characters from already made list.
+    #def that will create a new list with N characters from already made list.
     value_list = []
     for i in range(0, nTimes):
         value_list.append(list_choice[random.randint(0, len(list_choice) - 1)])
@@ -53,7 +53,11 @@ for i in range(0, len(password)):
     sortPassword += password[randomNumber]
     password.pop(randomNumber)
 
-print(sortPassword)
+#for test only
+# print(sortPassword)
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+#code to print password as a string
+yourPassword = ""
+for i in sortPassword:
+    yourPassword += i
+print(yourPassword)
