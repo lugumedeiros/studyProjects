27bcaab9092228ed11ba644580e6bd8c0b8a077b
lugumedeiros <140826713+lugumedeiros@users.noreply.github.com>
2023-08-18 23:42:53 -0300

    Update 100DBootCamp_PasswordGenerator.py

--- 100DBootCamp_PasswordGenerator.py
+++ 100DBootCamp_PasswordGenerator.py
@@ -1,4 +1,13 @@
 #Password Generator Project
+"""As usual in the 100 days bootcamp from Udemy, everyday we get a project for us to
+finish. In this one my personal objective was to make all code using basic random method,
+I personally like to do things this way, for me, it feels like I'm learning more.
+There's better ways to random everything like using choice method, but what is
+the fun in using it? lol.
+Maybe my next random code will be made from scratch using something simple
+like middle square algorithm, but I don't know yet how to implement a code
+that would randomize the seeds too.
+"""
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
