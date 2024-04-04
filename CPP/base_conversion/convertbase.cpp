0c997213559c561ac4509dfb21be9b520872db8e
Luiz Gustavo de Medeiros <140826713+lugumedeiros@users.noreply.github.com>
2024-04-03 23:45:43 -0300

    Update convertbase.cpp

--- CPP/base_conversion/convertbase.cpp
+++ CPP/base_conversion/convertbase.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <cmath>
 
-int main() {
+void number_to_base() {
 	unsigned long int value{};
 	unsigned long int base{};
-	std::cout << "Please, give the value and base wanted with a space in between: ";
+	std::cout << "Please, give the value and desired base with a space in between: ";
 	std::cin >> value >> base;
 	std::cout << std::endl;
-	if (base <= 1) { std::cout << "base less than 2 is not allowed."; exit; };
+	if (base <= 1) { std::cout << "base less than 2 is not allowed."; return; };
 	std::vector<unsigned long int> number{};
-	while (value >= base*base){
+	while (value >= base * base) {
 
 		number.push_back(value % base);
 		value = value / base;
@@ -17,20 +19,47 @@ int main() {
 	number.push_back(value % base);
 	number.push_back(value / base);
 
-	std::reverse(number.begin(),number.end());
+	std::reverse(number.begin(), number.end());
+	std::cout << "Result: ";
 	for (auto i : number) {
-		if (base != 16) {
 			std::cout << i << " ";
-		} else {
-			switch (i) {
-			case 10: std::cout << 'A' << " "; continue;
-			case 11: std::cout << 'B' << " "; continue;
-			case 12: std::cout << 'C' << " "; continue;
-			case 13: std::cout << 'D' << " "; continue;
-			case 14: std::cout << 'E' << " "; continue;
-			case 15: std::cout << 'F' << " "; continue;
-			default: std::cout << i << " ";
-			}
+	}
+};
+
+void base_to_number() {
+	unsigned long int value{};
+	unsigned long int base{};
+	std::cout << "Please, give the value and it's base with a space in between: ";
+	std::cin >> value >> base;
+	if (base <= 1) { std::cout << "base less than 2 is not allowed."; return; };
+	unsigned long int number{0};
+	unsigned int base_size{};
+	base_size = pow(10,std::to_string(base).length());
+
+	unsigned long int base_potency{ 1 };
+	while (value > base) {
+		unsigned int n = value % base_size;
+		value = value / base_size;
+		number += n * base_potency;
+		base_potency *= base;
+	}
+	number += value*base_potency;
+	std::cout << "Result: " << number;
+}
+
+int main() {
+	std::cout << "Simple number to base converter:";
+	while (true) {
+		std::cout << "\n\n1.Convert from decimal to any base.\n"
+			<< "2.Convert from any base to decimal.\n"
+			<< "3.Quit.\n"
+			<< "Answer: ";
+		unsigned int option{};
+		std::cin >> option;
+		switch (option) {
+			case 1: number_to_base(); continue;
+			case 2: base_to_number(); continue;
+			default: std::cout << "Invalid number.";
 		}
 	}
 	return 0;
