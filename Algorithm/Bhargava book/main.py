744691994a4a9330077589702f2981f4736ba55f
Luiz Gustavo de Medeiros <lugumedeiros@gmail.com>
2025-04-07 17:09:58 -0300

    Refactor sorting and searching algorithms into class methods; add binary search

--- Algorithm/Bhargava book/main.py
+++ Algorithm/Bhargava book/main.py
@@ -5,177 +5,193 @@ import random
 import time
 import heapq
 
-def instersort(lst):
-    for i in range(len(lst)):
-        for j in range(i):
-            pointer = (i - j)
-            left = lst[pointer - 1]
-            right = lst[pointer]
-            if left > right:
-                lst[pointer] = left
-                lst[pointer - 1] = right
-            else:
-                break
-    
-    return lst
-
-def quicksort(lst):
-    if len(lst) <= 1:
-        return lst
-    else:
-        pivot = lst[len(lst) // 2]
-        less = [x for x in lst if x < pivot]
-        equal = [x for x in lst if x == pivot]
-        more = [x for x in lst if x > pivot]
-        return quicksort(less) + equal + quicksort(more)
-
-def merge_sort(lst):
-    def sort(lst_a, lst_b):
-        new_lst = []
-        while len(lst_a) >= 1 and len(lst_b) >= 1:
-            if lst_a[0] <= lst_b[0]:
-                new_lst.append(lst_a[0])
-                lst_a.pop(0)
-            else:
-                new_lst.append(lst_b[0])
-                lst_b.pop(0)
-        
-        if len(lst_a) > 0:
-            return new_lst + lst_a
-        else:
-            return new_lst + lst_b
-
-    lst_size = len(lst)
-    if lst_size == 1:
-        return lst
-    else:
-        lst_a = lst[: lst_size//2]
-        lst_b = lst[lst_size//2 :]
-        return sort(merge_sort(lst_a), merge_sort(lst_b))
-    
-def bfs_search(graph, start_search, item_search):
-    # Tried to do my own just from concepts, not so bad I think
-    invalid_graph = []
-    search_graph = graph[start_search]
-    degree = 1
-    
-    while len(search_graph) > 0:
-        if item_search in search_graph:
-            print(f"item found!, {degree} items distant from start")
-            break
-        else:
-            temp_graph = search_graph.copy()
-            for key in search_graph:
-                invalid_graph.append(key)
-                temp_graph += graph[key]
-            new_graph = []
-            for key in temp_graph:
-                if not key in invalid_graph:
-                    new_graph.append(key)
-        search_graph = new_graph
-        degree += 1
-
-def bfs_search2(graph, start_search, item_search):
-    # This one I applied FIFO, first time I used this...
-    degree = 0
-    fifo = deque([(start_search, degree)]) # List of tuples
-    invalid_keys = set([start_search])
-
-    while len(fifo) > 0:
-        key, degree = fifo[0]
-        if key == item_search:
-            print(f"item found!, {degree} items distant from start")
-            return
-        else:
-            fifo.popleft()
-            for subkey in graph[key]:
-                if not subkey in invalid_keys:
-                    fifo.append((subkey, degree + 1))
-
-def dijkstra(graph, start):
-    """I tried to make my own code for the dijkstra algorithm but it ended like a bfs search with distance mapping
-    The main problem is that it will watse time because the order of searching is decided by the graph input,
-    and not about the shortest path first, but in the end works and it has the complexity of a bfs lol.
-    """
-
-    # Create a FIFO and visited blacklist set
-    fifo = deque([(start)])
-    visited = set([])
-    
-    # Create distance map with invalid dist.
-    distance = {node:float('inf') for node in graph}
-    distance[start] = 0
-
-    while len(fifo):
-        node = fifo.popleft()
-        visited.add(node)
-        curr_dist = distance[node]
-        for key, dist in graph[node]:
-            dist += curr_dist
-            # Test if current node was visited already
-            if key in visited:
-                pass
-            else:
-                fifo.append(key)
-                # Test if current declared distance is greater than new one
-                if distance[key] > dist:
-                    distance[key] = dist
+class Sort:
+    @staticmethod
+    def instersort(lst):
+        for i in range(len(lst)):
+            for j in range(i):
+                pointer = (i - j)
+                left = lst[pointer - 1]
+                right = lst[pointer]
+                if left > right:
+                    lst[pointer] = left
+                    lst[pointer - 1] = right
                 else:
-                    pass
-    return distance  
+                    break
+        
+        return lst
 
-def dijkstra2(graph, start):
-    """ I never user heapq before, so this was my poor implementation of "correct" dijkstra, chapgpt is screaming tho...
-    In the end this was potentially slower than my bfs version.
-    """
-    def get_smaller_value(map, blacklist):
-        smll_v = float('inf')
-        smll_k = None
-        for key, value in map.items():
-            if not (key in blacklist) and (value < smll_v):
-                smll_v = value
-                smll_k = key
-        if smll_k == None:
-            pass
-        return smll_k
+    def quicksort(lst):
+        if len(lst) <= 1:
+            return lst
+        else:
+            pivot = lst[len(lst) // 2]
+            less = [x for x in lst if x < pivot]
+            equal = [x for x in lst if x == pivot]
+            more = [x for x in lst if x > pivot]
+            return Sort.quicksort(less) + equal + Sort.quicksort(more)
 
-    set_visited = set([]) # List with all searched nodes
-    map_dist = {node:float('inf') for node in graph} # List with the updated distances from start
-    map_dist[start] = 0
-
-    while len(set_visited) < len(map_dist):
-        node = get_smaller_value(map_dist, set_visited)
-        if node is None: break
-        set_visited.add(node)
-        current_dist = map_dist[node]
-
-        for sub_node, sub_dist in graph[node]:
-            sub_dist += current_dist
-            if sub_node in set_visited:
-                pass
+    def merge_sort(lst):
+        def sort(lst_a, lst_b):
+            new_lst = []
+            while len(lst_a) >= 1 and len(lst_b) >= 1:
+                if lst_a[0] <= lst_b[0]:
+                    new_lst.append(lst_a[0])
+                    lst_a.pop(0)
+                else:
+                    new_lst.append(lst_b[0])
+                    lst_b.pop(0)
+            
+            if len(lst_a) > 0:
+                return new_lst + lst_a
             else:
+                return new_lst + lst_b
+
+        lst_size = len(lst)
+        if lst_size == 1:
+            return lst
+        else:
+            lst_a = lst[: lst_size//2]
+            lst_b = lst[lst_size//2 :]
+            return sort(self.merge_sort(lst_a), self.merge_sort(lst_b))
+    
+class Search:
+    @staticmethod
+    def bfs_search(graph, start_search, item_search):
+        # Tried to do my own just from concepts, not so bad I think
+        invalid_graph = []
+        search_graph = graph[start_search]
+        degree = 1
+        
+        while len(search_graph) > 0:
+            if item_search in search_graph:
+                print(f"item found!, {degree} items distant from start")
+                break
+            else:
+                temp_graph = search_graph.copy()
+                for key in search_graph:
+                    invalid_graph.append(key)
+                    temp_graph += graph[key]
+                new_graph = []
+                for key in temp_graph:
+                    if not key in invalid_graph:
+                        new_graph.append(key)
+            search_graph = new_graph
+            degree += 1
+
+    def bfs_search2(graph, start_search, item_search):
+        # This one I applied FIFO, first time I used this...
+        degree = 0
+        fifo = deque([(start_search, degree)]) # List of tuples
+        invalid_keys = set([start_search])
+
+        while len(fifo) > 0:
+            key, degree = fifo[0]
+            if key == item_search:
+                print(f"item found!, {degree} items distant from start")
+                return
+            else:
+                fifo.popleft()
+                for subkey in graph[key]:
+                    if not subkey in invalid_keys:
+                        fifo.append((subkey, degree + 1))
+
+    def dijkstra(graph, start):
+        """I tried to make my own code for the dijkstra algorithm but it ended like a bfs search with distance mapping
+        The main problem is that it will watse time because the order of searching is decided by the graph input,
+        and not about the shortest path first, but in the end works and it has the complexity of a bfs lol.
+        """
+
+        # Create a FIFO and visited blacklist set
+        fifo = deque([(start)])
+        visited = set([])
+        
+        # Create distance map with invalid dist.
+        distance = {node:float('inf') for node in graph}
+        distance[start] = 0
+
+        while len(fifo):
+            node = fifo.popleft()
+            visited.add(node)
+            curr_dist = distance[node]
+            for key, dist in graph[node]:
+                dist += curr_dist
+                # Test if current node was visited already
+                if key in visited:
+                    pass
+                else:
+                    fifo.append(key)
+                    # Test if current declared distance is greater than new one
+                    if distance[key] > dist:
+                        distance[key] = dist
+                    else:
+                        pass
+        return distance  
+
+    def dijkstra2(graph, start):
+        """ I never user heapq before, so this was my poor implementation of "correct" dijkstra, chapgpt is screaming tho...
+        In the end this was potentially slower than my bfs version.
+        """
+        def get_smaller_value(map, blacklist):
+            smll_v = float('inf')
+            smll_k = None
+            for key, value in map.items():
+                if not (key in blacklist) and (value < smll_v):
+                    smll_v = value
+                    smll_k = key
+            if smll_k == None:
+                pass
+            return smll_k
+
+        set_visited = set([]) # List with all searched nodes
+        map_dist = {node:float('inf') for node in graph} # List with the updated distances from start
+        map_dist[start] = 0
+
+        while len(set_visited) < len(map_dist):
+            node = get_smaller_value(map_dist, set_visited)
+            if node is None: break
+            set_visited.add(node)
+            current_dist = map_dist[node]
+
+            for sub_node, sub_dist in graph[node]:
+                sub_dist += current_dist
+                if sub_node in set_visited:
+                    pass
+                else:
+                    if sub_dist < map_dist[sub_node]:
+                        map_dist[sub_node] = sub_dist
+
+        return map_dist
+                    
+    def dijkstra3(graph, start):
+        """This is the correct python code for "dijksjktsra" i think... I used heapq this time, as adviced by chatgpt."""
+        map_dist = {node:float('inf') for node in graph}
+        map_dist[start] = 0
+        to_visit = [(0, start)]
+
+        while len(to_visit) > 0:
+            current_dist, node = heapq.heappop(to_visit)
+
+            for sub_node, sub_dist in graph[node]:
+                sub_dist += current_dist
+
                 if sub_dist < map_dist[sub_node]:
                     map_dist[sub_node] = sub_dist
+                    heapq.heappush(to_visit, (sub_dist, sub_node))
 
-    return map_dist
-                
-def dijkstra3(graph, start):
-    """This is the correct python code for "dijksjktsra" i think... I used heapq this time, as adviced by chatgpt."""
-    map_dist = {node:float('inf') for node in graph}
-    map_dist[start] = 0
-    to_visit = [(0, start)]
+        return map_dist
 
-    while len(to_visit) > 0:
-        current_dist, node = heapq.heappop(to_visit)
-
-        for sub_node, sub_dist in graph[node]:
-            sub_dist += current_dist
-
-            if sub_dist < map_dist[sub_node]:
-                map_dist[sub_node] = sub_dist
-                heapq.heappush(to_visit, (sub_dist, sub_node))
-
-    return map_dist
+    def bin_search(lst, value):
+        idx = len(lst) // 2
+        midd = lst[idx]
+        if midd == value:
+            return idx
+        elif len(lst) == 1:
+            return None
+        elif midd < value:
+            return Search.bin_search(lst[idx:], value)
+        else:
+            return Search.bin_search(lst[:idx], value)
 
 def get_full_graph():
     d = {}
@@ -219,47 +235,10 @@ def get_node_dist_graph():
     return graph_large
 
 GRAPH_W = get_node_dist_graph()
+LISTA = [random.randint(0, 100) for _ in range(10)]
 
-# s = time.perf_counter()
-# r = dijkstra(GRAPH_W.copy(), "N0")
-# e = time.perf_counter() - s
-# print(f"Dijskra1: in {e}s\n{r}\n")
-
-# s = time.perf_counter()
-# r = dijkstra2(GRAPH_W.copy(), "N0")
-# e = time.perf_counter() - s
-# print(f"Dijskra2: in {e}s\n{r}\n")
-
-# s = time.perf_counter()
-# r = dijkstra3(GRAPH_W.copy(), "N0")
-# e = time.perf_counter() - s
-# print(f"Dijskra3: in {e}s\n{r}\n")
-
-# GRAPH = get_full_graph()
-
-# s = time.perf_counter()
-# r = bfs_search(GRAPH.copy(), "YOU", "JAIR")
-# e = time.perf_counter() - s
-# print(f"BFS: in {e}s")
-
-# s = time.perf_counter()
-# r = bfs_search2(GRAPH.copy(), "YOU", "JAIR")
-# e = time.perf_counter() - s
-# print(f"BFS: in {e}s")
-
-LISTA = [random.randint(0, 10) for _ in range(10)]
-
+n_list = Sort.quicksort(LISTA.copy())
 s = time.perf_counter()
-r = instersort(LISTA.copy())
+r = Search.bin_search(n_list, 3)
 e = time.perf_counter() - s
-print(f"insertSort: in {e}s\nresult: {r}")
-
-# s = time.perf_counter()
-# r = merge_sort(LISTA.copy())
-# e = time.perf_counter() - s
-# print(f"Merge: in {e}s")
-
-# s = time.perf_counter()
-# r = quicksort(LISTA.copy())
-# e = time.perf_counter() - s
-# print(f"QuickSort: in {e}s")
+print(f"BFS: in {e}s\nResult: {r}")
